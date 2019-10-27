4169308650239d302fdd7a37c0eb95711eff3d75
yszk0123 <yszk0123@users.noreply.github.com>
2019-10-27 20:46:55 +0000

    Automated deployment: Sun Oct 27 20:46:55 UTC 2019 3cc2fa92802e6199e8c93899d06f42bc8683f916

---
+++ precache-manifest.078c51770ab2fd3dfb39d6e6a85e4fce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128b800a1ae8a83141c6a2cb9b4038c1",
+    "url": "/memo/index.html"
+  },
+  {
+    "revision": "3b1d19d249cc36a6b107",
+    "url": "/memo/static/css/main.b9751508.chunk.css"
+  },
+  {
+    "revision": "a5052ffbc049e0da0a5b",
+    "url": "/memo/static/js/2.e6bd2805.chunk.js"
+  },
+  {
+    "revision": "3b1d19d249cc36a6b107",
+    "url": "/memo/static/js/main.52ec0c5f.chunk.js"
+  },
+  {
+    "revision": "834646fc27e75d3b0757",
+    "url": "/memo/static/js/runtime-main.ed0d23f5.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/memo/precache-manifest.078c51770ab2fd3dfb39d6e6a85e4fce.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/memo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
