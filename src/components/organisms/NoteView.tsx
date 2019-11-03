959ed221f58d29788948580c6e035914acbb06b5
Yuji Suzuki <y0suzuki0123@gmail.com>
2019-11-03 18:10:33 +0900

    Remove "Remove" button from /

--- src/components/organisms/NoteView.tsx
+++ src/components/organisms/NoteView.tsx
@@ -1,8 +1,7 @@
 import Link from 'next/link';
-import React, { useCallback, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import styled from 'styled-components';
 import { Note } from '../../types/NoteType';
-import { SecondaryButton } from '../atoms/Button';
 import { LineBreakableText } from '../atoms/LineBreakableText';
 import { ListItem } from '../atoms/ListItem';
 import { pad2 } from './pad2';
@@ -17,15 +16,6 @@ interface Props {
 export const NoteView: React.FunctionComponent<Props> = ({ className, style, note, onRemove }) => {
   const createdAt = useMemo(() => formatTime(note.createdAt), [note]);
 
-  const handleRemove = useCallback(
-    (event: React.MouseEvent) => {
-      onRemove(note.id);
-      event.preventDefault();
-      event.stopPropagation();
-    },
-    [note, onRemove],
-  );
-
   return (
     <Link href="/notes/[id]" as={`/notes/${note.id}`}>
       <Container className={className} style={style}>
@@ -35,7 +25,6 @@ export const NoteView: React.FunctionComponent<Props> = ({ className, style, not
           </Text>
           <CreatedAt>{createdAt}</CreatedAt>
         </Content>
-        <RemoveButton onClick={handleRemove}>Remove</RemoveButton>
       </Container>
     </Link>
   );
@@ -67,15 +56,10 @@ const Text = styled.p`
 `;
 
 const CreatedAt = styled.p`
+  align-self: flex-end;
   font-size: var(--font-sm);
-  min-width: 120px;
   color: var(--color-button-secondary-text--reverse);
   margin: 0;
-`;
-
-const RemoveButton = styled(SecondaryButton)`
-  margin-left: var(--space);
-  align-self: center;
 `;
 
 function formatTime(timestamp: number): string {
