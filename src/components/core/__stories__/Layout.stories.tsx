7c137b2c40ab831b91a86432306d0fd45acae7f7
Yuji Suzuki <y0suzuki0123@gmail.com>
2019-11-04 00:25:20 +0900

    Add stories for Layout

---
+++ src/components/core/__stories__/Layout.stories.tsx
@@ -0,0 +1,15 @@
+import { action } from '@storybook/addon-actions';
+import React from 'react';
+import { Layout } from '../Layout';
+
+export default {
+  title: 'core/Layout',
+};
+
+export const LayoutStory = () => {
+  return (
+    <Layout user={null} onLogin={action('onLogin')} onLogout={action('onLogout')}>
+      Content
+    </Layout>
+  );
+};
