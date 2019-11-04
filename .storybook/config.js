8f9949a56d0dfee5d9144317f0f16fc910d883ec
Yuji Suzuki <y0suzuki0123@gmail.com>
2019-11-04 19:02:36 +0900

    Fix modal in storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,6 +5,14 @@ import styled from 'styled-components';
 
 import './hacks/nextRouterMock';
 
+// **append** elements to the body
+// preview-body.html **prepend** elements
+if (!document.querySelector('#modal')) {
+  const modal = document.createElement('div');
+  modal.id = 'modal';
+  document.body.appendChild(modal);
+}
+
 // automatically import all files ending in *.stories.{ts,tsx}
 configure(require.context('../src/components', true, /\.stories\.tsx?$/), module);
 
